54fdbb1c6eca5387c3de7793b90aa7632e90b0a4
Matthew A. Bardin <43657205+mbardin@users.noreply.github.com>
2023-03-14 22:23:32 -0500

    Update Cyberinet_V1.3.ino

--- Arduino V 1.3/Cyberinet_V1.3/Cyberinet_V1.3.ino
+++ Arduino V 1.3/Cyberinet_V1.3/Cyberinet_V1.3.ino
@@ -1,3 +1,5 @@
+// This is a Wired only version convert to SerialBT commands to make wireless
+// I believe that startup error is due to wireing a pin incorrectly since it connects when not connected to the board.
 
 //libraries to include
 #include <SparkFun_SDP3x_Arduino_Library.h>
@@ -27,7 +29,6 @@ int16_t AcX, AcY, AcZ, GyX, GyY, GyZ; // values for 6050
 // pValues are previously sensed values. Used to reduce noise in signal.
 int pB1 = 0;
 int pB2 = 0;
-int msgState;
 int button1State = 0;
 int button2State = 0;
 
@@ -44,7 +45,7 @@ void setup() {
   pinMode(button2, INPUT_PULLUP);
 
   Serial.begin(115200); // making sure this is before serialBT.begin ?
-  SerialBT.begin("Cyberinet V.1.3"); // Device name that will appear on computer
+  //SerialBT.begin("Cyberinet V.1.3"); // Device name that will appear on computer
   delay(1000); // pause before checking
   // various startup and checks
   gyroStartup();
@@ -96,14 +97,14 @@ void get6050() {
   rotY = GyY *  0.007633587786;
   rotZ = GyZ *  0.007633587786;
 
-  SerialBT.print("GyroX ");
-  SerialBT.println(rotX);
+  Serial.print("GyroX ");
+  Serial.println(rotX);
 
-  SerialBT.print("GyroY ");
-  SerialBT.println(rotY);
+  Serial.print("GyroY ");
+  Serial.println(rotY);
 
-  SerialBT.print("GyroZ ");
-  SerialBT.println(rotZ);
+  Serial.print("GyroZ ");
+  Serial.println(rotZ);
 
   //  gForceX = AcX / 16384.0; // offsets for gravity
   //  gForceY = AcY / 16384.0;
@@ -113,14 +114,14 @@ void get6050() {
   gForceY = AcY * 0.00006103515;
   gForceZ = AcZ * 0.00006103515;
 
-  SerialBT.print("AcelX ");
-  SerialBT.println(gForceX);
+  Serial.print("AcelX ");
+  Serial.println(gForceX);
 
-  SerialBT.print("AclY ");
-  SerialBT.println(gForceY);
+  Serial.print("AclY ");
+  Serial.println(gForceY);
 
-  SerialBT.print("AcelZ ");
-  SerialBT.println(gForceZ);
+  Serial.print("AcelZ ");
+  Serial.println(gForceZ);
 
 }
 
@@ -133,14 +134,14 @@ void getButtons() { // currently whenever pressed. look into holding and release
   button2State = digitalRead(button2);
   // transmit  values and change LED when the button changes state
   if (pB1 != button1State) {
-    SerialBT.print("Button1 ");
-    SerialBT.println(button1State);
+    Serial.print("Button1 ");
+    Serial.println(button1State);
     digitalWrite(b1LED, button1State);
   }
   // buttons are independant of each other
   if (pB2 != button2State) {
-    SerialBT.print("Button2 ");
-    SerialBT.println(button2State);
+    Serial.print("Button2 ");
+    Serial.println(button2State);
     digitalWrite(b1LED, button2State);
   }
 }
@@ -154,14 +155,14 @@ void getAir() {
 
   // transmit values if they are above a range
   if (abs(pDiff) - abs(diffPressure) > 0.5) { // check the ranges for more effective values
-    SerialBT.print(F("AirP "));
-    SerialBT.println(diffPressure, 2);
+    Serial.print(F("AirP "));
+    Serial.println(diffPressure, 2);
   }
 
 
   // transmits temp every loop because values will change too slowly to be effective
-  SerialBT.print(F("Temp "));
-  SerialBT.println(temperature, 2);
+  Serial.print(F("Temp "));
+  Serial.println(temperature, 2);
 }
 
 // startup functions
@@ -216,27 +217,18 @@ void airflowStartup() {
   }
 
   airFlow.startContinuousMeasurement(true, true);
-  SerialBT.println("SDP31 Setup Complete");
+  Serial.println("SDP31 Setup Complete");
 }
 
 void startLights() { // flashes startup LED in apecific pattern
   digitalWrite(pwrLED, HIGH);
-  ledToggle();
+  digitalWrite(msgLED, HIGH);
   delay(500);
-  ledToggle();
+  digitalWrite(msgLED, LOW);
   delay(500);
-  ledToggle();
+  digitalWrite(msgLED, HIGH);
   delay(500);
-  ledToggle();
+  digitalWrite(msgLED, LOW);
   delay(1000);
-  ledToggle();
-}
-
-void ledToggle() { // toggles message LED on board
-  if (msgState == 1) {
-    msgState = 0;
-  } else {
-    msgState = 1;
-  }
-  digitalWrite(msgLED, msgState);
+  digitalWrite(msgLED, HIGH);
 }
