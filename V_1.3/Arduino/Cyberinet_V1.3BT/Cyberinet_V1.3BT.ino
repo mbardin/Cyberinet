4e77dcdd2d75873a7e04ef208badd01168e1f31a
Matthew A. Bardin <43657205+mbardin@users.noreply.github.com>
2023-04-07 10:23:43 -0500

    cleaned up comments

--- V_1.3/Arduino/Cyberinet_V1.3BT/Cyberinet_V1.3BT.ino
+++ V_1.3/Arduino/Cyberinet_V1.3BT/Cyberinet_V1.3BT.ino
@@ -1,4 +1,11 @@
-
+/*
+    Micro Controller Code for the Cyberinet Version 1.3
+   Code designed for the ESP-32 WROOM DEVKIT V1 in the Arduino IDE.
+   All Serial Communications are over Bluetooth.
+   Code may be used or reworked with Attribution. Altering code present on Cyberinet unit is not recommended.
+   Code and Other Matierials for the Cyberinet can be found at https://github.com/mbardin/Cyberinet
+   Code and Other Marerials made by Matthew A. Bardin [2023] (https://matthewbardin.com)
+*/
 
 //libraries to include
 #include <SparkFun_SDP3x_Arduino_Library.h>
@@ -6,10 +13,6 @@
 #include <BluetoothSerial.h>
 #include <MPU6050.h>
 
-// Check if Bluetooth configs are enabled
-//#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-//#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-//#endif
 
 // create instances of the libraries above
 BluetoothSerial SerialBT;
@@ -26,15 +29,16 @@ const int button2 = 14;
 const int b1LED = 26;
 const int b2LED = 27;
 
+// MPU 6050 values
 const int MPU_ADDR = 0x68; // I2C address of the MPU-6050
 int16_t AcX, AcY, AcZ, GyX, GyY, GyZ; // values for 6050
+float rotX, rotY, rotZ, gForceX, gForceY, gForceZ; // store mapped values for transmitting
 
-// pValues are previously sensed values. Used to reduce noise in signal.
+// stores current button state
+int button1State = 1;
+int button2State = 1;
 
-int button1State = 0;
-int button2State = 0;
-
-float pDiff, pTemp, rotX, rotY, rotZ, gForceX, gForceY, gForceZ;
+// store SDP31 values for transmitting
 float diffPressure = 0.0;
 float temperature = 0.0;
 
@@ -46,13 +50,12 @@ void setup() {
   pinMode(button2, INPUT_PULLUP);
 
   Serial.begin(115200);
-  SerialBT.begin("CyberinetV13"); // Device name
+  SerialBT.begin("CyberinetV13"); // Device name. no spaces or punctuation
   delay(1000); // pause before checking
   // various startup and checks
   gyroStartup();
   airflowStartup();
   // device must be restarted if optional expansions are added
-  // expansionCheck(); // make function to chekc for additoinal expansions in future versions
   // if everything clears
   startLights();
 }
@@ -62,56 +65,54 @@ void setup() {
 void loop() { // check the sensors and transmit each loop.
   get6050();
   getButtons();
-  getAir(); // put 6050 and sdp31 on ends of sensor checks to give time between accessing the I2C pin.
+  getAir(); // put 6050 and sdp31 on ends of sensor checks
 }
 
 
 void get6050() {
-  //map values here?
 
+  ////////////Collect Data
   Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.write(0x3B);  // wake up device
   Wire.endTransmission(true);
 
   Wire.requestFrom(MPU_ADDR, 6); //Request Accel Registers (3B - 40)
   while (Wire.available() < 6);
-  AcX = Wire.read() << 8 | Wire.read();
-  AcY = Wire.read() << 8 | Wire.read();
-  AcZ = Wire.read() << 8 | Wire.read();
+  AcX = Wire.read() << 8 | Wire.read(); // operator first bitshifts the sensor value by 8 bits,
+  AcY = Wire.read() << 8 | Wire.read(); // then does a bitwise OR to compare to raw data
+  AcZ = Wire.read() << 8 | Wire.read(); // enbd result is a combined original and bitshift
 
   Wire.beginTransmission(MPU_ADDR); //I2C address of the MPU
   Wire.write(0x43); //Starting register for Gyro Readings
   Wire.endTransmission();
-  Wire.requestFrom(MPU_ADDR, 6); //Request Gyro Registers (43 - 48)
+  Wire.requestFrom(MPU_ADDR, 6); //Request Gyro/Accel Registers (43 - 48)
   while (Wire.available() < 6);
   GyX = Wire.read() << 8 | Wire.read();
   GyY = Wire.read() << 8 | Wire.read();
   GyZ = Wire.read() << 8 | Wire.read();
 
-  //  rotX = GyX / 131.0; // offsets for gravity
-  //  rotY = GyY / 131.0;
-  //  rotZ = GyZ / 131.0;
-  rotX = GyX *  0.007633587786; // mult is better for computations
+
+  ////////////Format Data
+  rotX = GyX *  0.007633587786; // gravity and rotation offsets
   rotY = GyY *  0.007633587786;
   rotZ = GyZ *  0.007633587786;
-digitalWrite(msgLED, HIGH);
-  SerialBT.print("GyroX ");
-  SerialBT.println(rotX);
 
-  SerialBT.print("GyroY ");
-  SerialBT.println(rotY);
-
-  SerialBT.print("GyroZ ");
-  SerialBT.println(rotZ);
-digitalWrite(msgLED, LOW);
-  //  gForceX = AcX / 16384.0; // offsets for gravity
-  //  gForceY = AcY / 16384.0;
-  //  gForceZ = AcZ / 16384.0;
-
-  gForceX = AcX * 0.00006103515; // mult is better for computations
+  gForceX = AcX * 0.00006103515;
   gForceY = AcY * 0.00006103515;
   gForceZ = AcZ * 0.00006103515;
-digitalWrite(msgLED, HIGH);
+
+
+  ////////////Transmit Data
+  digitalWrite(msgLED, HIGH); // turn on LED when when transmitting
+  SerialBT.print("GyroX "); // label
+  SerialBT.println(rotX); // value
+
+  SerialBT.print("GyroY "); // label
+  SerialBT.println(rotY); // value
+
+  SerialBT.print("GyroZ "); // etc.
+  SerialBT.println(rotZ);
+
   SerialBT.print("AccelX ");
   SerialBT.println(gForceX);
 
@@ -120,71 +121,65 @@ digitalWrite(msgLED, HIGH);
 
   SerialBT.print("AccelZ ");
   SerialBT.println(gForceZ);
-digitalWrite(msgLED, LOW);
+  digitalWrite(msgLED, LOW);// turn off led when done transmitting
 }
 
-void getButtons() { // currently whenever pressed. look into holding and release options
+void getButtons() {
+  ////////////Collect Data
   button1State = digitalRead(button1);
   button2State = digitalRead(button2);
-  // transmit  values and change LED each frame. May need tweaking.
-   digitalWrite(msgLED, HIGH);
-    
-    SerialBT.print("B1 ");
-    SerialBT.println(button1State);
-    digitalWrite(b1LED, button1State);
-  // buttons are independant of each other
-    SerialBT.print("B2 ");
-    SerialBT.println(button2State);
-    digitalWrite(b1LED, button2State);
-  digitalWrite(msgLED, LOW);
+
+  ////////////Transmit Data (every frame. CNET.receive Max Object filters out repetitions.)
+  digitalWrite(msgLED, HIGH); // turn LED on when tranmitting data
+  SerialBT.print("B1 "); // label
+  SerialBT.println(button1State); // value
+  digitalWrite(b1LED, button1State); // on-board led state matches button state
+  // repeat for other button
+  SerialBT.print("B2 ");
+  SerialBT.println(button2State);
+  digitalWrite(b1LED, button2State);
+  digitalWrite(msgLED, LOW); // turn off led when done transmitting
 }
 
 void getAir() {
-  // store previous values
-  pDiff = diffPressure;
-  pTemp = temperature;
+  ////////////Collect Data
+  airFlow.readMeasurement(&diffPressure, &temperature);
 
-  airFlow.readMeasurement(&diffPressure, &temperature); // get values
+  ////////////Transmit Data
+  digitalWrite(msgLED, HIGH); // turn on LED when tranmitting
+  SerialBT.print(F("AirP ")); // lebel
+  SerialBT.println(diffPressure, 2); // value
 
-  // transmit values if they are above a range
-//  if (abs(pDiff) - abs(diffPressure) > 0.5) { // check the ranges for more effective values
-    digitalWrite(msgLED, HIGH);
-    SerialBT.print(F("AirP "));
-    SerialBT.println(diffPressure, 2);
-//  }
-digitalWrite(msgLED, LOW);
-
-  // transmits temp every loop because values will change too slowly to be effective
-  digitalWrite(msgLED, HIGH);
   SerialBT.print(F("Temp "));
   SerialBT.println(temperature, 2);
-  digitalWrite(msgLED, LOW);
+  digitalWrite(msgLED, LOW); // turn of LED when done transmitting
 }
 
-// startup functions
 
+// startup functions
 void gyroStartup() {
   Wire.begin(21, 22, 100000); // sda, scl, clock speed
   Wire.beginTransmission(MPU_ADDR);
   Wire.write(0x6B);  // PWR_MGMT_1 register
   Wire.write(0);     // (wakes up the MPU−6050)
   Wire.endTransmission(true);
-  SerialBT.println("6050 Setup Complete");
+  SerialBT.println("MPU-6050 Setup Complete");
 }
 
 void airflowStartup() {
-  // Initialize sensor
+  // Initialize sensor. Stops if connection cannot be made
   if (airFlow.begin() == false)
   {
     SerialBT.println(F("SDP31 not detected. Check connections. Freezing..."));
     while (1);
   }
 
-  airFlow.startContinuousMeasurement(true, true);
+  airFlow.startContinuousMeasurement(true, true); // set to continuous measurement mode
   SerialBT.println("SDP31 Setup Complete");
 }
 
 void startLights() { // flashes startup LED in apecific pattern
+  // process takes 3.5 seconds
   digitalWrite(pwrLED, HIGH);
   digitalWrite(msgLED, HIGH);
   delay(500);
@@ -196,5 +191,5 @@ void startLights() { // flashes startup LED in apecific pattern
   delay(1000);
   digitalWrite(msgLED, HIGH);
   delay(1000);
-  digitalWrite(msgLED, LOW);
+  digitalWrite(msgLED, LOW); // turns off LED so it can be activated when tranmitting.
 }
